87c626d185e797c0207d396d164eddf348b19566
SravaniKonathala <63584201+SravaniKonathala@users.noreply.github.com>
2020-08-04 19:51:15 +0100

    Update TestRunner.java
    Update

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -6,7 +6,7 @@ import org.testng.annotations.DataProvider;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = ".", dryRun = false,strict = true,format = {"pretty", "html:target/Destination"} )
+@CucumberOptions(features = ".", dryRun = false,strict = true,format = {"pretty", "target/Destination"} )
 public class TestRunner extends AbstractTestNGCucumberTests {
     @Override
     @DataProvider(parallel = true)
